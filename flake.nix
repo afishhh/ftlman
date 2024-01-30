f6b21c72fc1d52b80ee657345e671ff96cda4215
Fishhh <hubert.gluchowski19@gmail.com>
2024-01-30 02:22:12 +0100

    Add a package to the nix flake

--- flake.nix
+++ flake.nix
@@ -1,32 +1,69 @@
 {
   description = "A basic flake";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    with flake-utils.lib;
-    eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (pkgs) lib;
-      in {
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; with pkgs.xorg; [
-            bashInteractive
-            pkg-config
-          ];
-          buildInputs = with pkgs; with pkgs.xorg; [
-            libX11
-            libXcursor
-            libXrandr
-            libXi
-            fontconfig
-            openssl
-          ];
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages =
+        let
+          inherit (fenix.packages.${system}.latest) toolchain;
+          pkgs = nixpkgs.legacyPackages.${system};
+          runtimeLibs = "/run/opengl-driver/lib/:${pkgs.lib.makeLibraryPath (with pkgs; [ libGL libGLU libxkbcommon ])}";
 
-          shellHook = ''
-            export LD_LIBRARY_PATH=/run/opengl-driver/lib/:${lib.makeLibraryPath (with pkgs; [ libGL libGLU libxkbcommon ])}
+        in
+        rec {
+          unwrapped = (pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          }).buildRustPackage {
+            pname = "ftlman-unwrapped";
+            version = "0.1.0";
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+
+            buildInputs = with pkgs; with pkgs.xorg; [
+              libX11
+              libXcursor
+              libXrandr
+              libXi
+              fontconfig
+              openssl
+            ];
+
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                # silpkg's internal macros crate
+                # buildRustPackage doesn't seem to deal with such subpackages seamlessly
+                "macros-0.0.0" = "sha256-3z553VbZRNpO8vtmZsBbqsW3IOLdrYcurHtX+Dml2I0=";
+              };
+            };
+
+            shellHook = ''
+              export LD_LIBRARY_PATH=${runtimeLibs}
+            '';
+          };
+          default = pkgs.runCommandNoCC "ftlman"
+            {
+              pname = "ftlman";
+              inherit (unwrapped) version;
+
+              nativeBuildInputs = [ pkgs.makeWrapper ];
+            } ''
+            makeWrapper ${unwrapped}/bin/ftlman $out/bin/ftlman --suffix LD_LIBRARY_PATH : ${runtimeLibs}
           '';
         };
-      });
+        devShells.default = self.packages.${system}.unwrapped;
+    });
 }
