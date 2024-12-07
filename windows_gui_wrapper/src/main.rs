877d0a0c92e1f9e845fd85919dd873259b320788
Fishhh <hubert.gluchowski19@gmail.com>
2024-12-07 23:40:04 +0100

    Make gui wrapper try both `ftlman.com` and `ftlman.exe`

--- windows_gui_wrapper/src/main.rs
+++ windows_gui_wrapper/src/main.rs
@@ -1,17 +1,33 @@
 #![windows_subsystem = "windows"]
 use std::{fmt::Write as _, os::windows::process::CommandExt, process::Child};
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use winapi::um::winuser::{MessageBoxW, MB_ICONERROR, MB_OK};
 
 fn run() -> Result<Child, anyhow::Error> {
     let exe = std::env::current_exe().context("Failed to get path to current executable")?;
+    let exe_filename = exe
+        .file_name()
+        .context("Executable has no filename (should never happen!)")?;
     let dir = exe.parent().context("Current executable path has no parent")?;
-    std::process::Command::new(dir.join("ftlman.com"))
-        .creation_flags(0x00000008)
-        .args(std::env::args_os().skip(1))
-        .spawn()
-        .context("Failed to execute ftlman.com")
+
+    const NAMES: &[&str] = &["ftlman.com", "ftlman.exe"];
+    for name in NAMES.iter().copied() {
+        if exe_filename == name {
+            continue;
+        }
+
+        let path = dir.join(name);
+        if path.exists() {
+            return std::process::Command::new(path)
+                .creation_flags(0x00000008)
+                .args(std::env::args_os().skip(1))
+                .spawn()
+                .context("Failed to execute ftlman.com");
+        }
+    }
+
+    bail!("Failed to find ftlman executable (tried: {NAMES:?})")
 }
 
 fn encode_wstr(text: &str) -> Vec<u16> {
@@ -26,8 +42,8 @@ fn main() {
             _ = write!(description, "\n{cause}");
         }
 
-        let _ = description.write_str("\n\nMake sure ftlman.exe is in the same directory as ftlman.com");
-        let _ = description.write_str(" and that you don't run this file directly from a compressed archive");
+        description.push_str("\n\nMake sure ftlman.exe is in the same directory as ftlman.com");
+        description.push_str(" and that you don't run this file directly from a compressed archive");
 
         let description = encode_wstr(&description);
         let title = encode_wstr("Wrapper Error");
