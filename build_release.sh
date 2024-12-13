865e000f20c17e1d58843afe51f8a6a15d2ac755
Fishhh <hubert.gluchowski19@gmail.com>
2024-12-14 00:27:28 +0100

    Change Windows executable names

--- build_release.sh
+++ build_release.sh
@@ -9,10 +9,10 @@ cross +nightly build --target x86_64-pc-windows-gnu --release
 [[ -e release ]] || mkdir release
 cd release
 
-ln ../target/x86_64-pc-windows-gnu/release/ftlman.exe ftlman.com
-ln ../target/x86_64-pc-windows-gnu/release/windows_gui_wrapper.exe ftlman.exe
-7z a ftlman-x86_64-pc-windows-gnu.zip ftlman.com ftlman.exe
-rm ftlman.com ftlman.exe
+ln ../target/x86_64-pc-windows-gnu/release/ftlman.exe ftlman.exe
+ln ../target/x86_64-pc-windows-gnu/release/windows_gui_wrapper.exe ftlman_gui.exe
+7z a ftlman-x86_64-pc-windows-gnu.zip ftlman_gui.exe ftlman.exe
+rm ftlman_gui.exe ftlman.exe
 
 ln ../target/x86_64-unknown-linux-gnu/release/ftlman ftlman
 tar cvaf ftlman-x86_64-unknown-linux-gnu.tar.gz ftlman
