63311bc017717c168c05367c19e1b51d80911bf5
Hubert Głuchowski <hubert.gluchowski19@gmail.com>
2025-01-24 16:28:42 +0100

    Add search box to XML sandbox

--- src/gui/sandbox.rs
+++ src/gui/sandbox.rs
@@ -1,9 +1,13 @@
-use std::{path::Path, sync::Arc};
+use std::{ops::Range, path::Path, sync::Arc};
 
 use anyhow::{Error, Result};
-use eframe::egui::{self, Ui};
+use eframe::egui::{
+    self, scroll_area, text::CCursor, text_selection::visuals::paint_text_selection, vec2, Color32, Id, Layout,
+    TextEdit, Ui,
+};
 use egui_extras::syntax_highlighting;
 use poll_promise::Promise;
+use regex::Regex;
 use silpkg::sync::Pkg;
 
 use crate::{apply, l, render_error_chain};
@@ -22,7 +26,11 @@ pub struct Sandbox {
     current_file: Option<CurrentFile>,
     patcher: Option<Promise<Result<String, Error>>>,
     output: Option<Output>,
-    output_find_box: Option<String>,
+    output_find_box: (String, Option<Regex>, usize),
+    output_find_matches: Vec<Range<usize>>,
+    output_find_invalidated: bool,
+    // HACK: I don't even want to write about this anymore.
+    output_scroll_id: Option<Id>,
 
     // Whether the patch XML was changed since the last update was ran.
     needs_update: bool,
@@ -62,7 +70,10 @@ impl Sandbox {
             current_file: None,
             patcher: None,
             output: None,
-            output_find_box: Some("9".to_string()),
+            output_find_box: (String::new(), None, 0),
+            output_find_matches: Vec::new(),
+            output_find_invalidated: false,
+            output_scroll_id: None,
             needs_update: false,
         }
     }
@@ -160,7 +171,8 @@ impl WindowState for Sandbox {
                 Ok(Ok(patched)) => Output::ResultXml(patched),
                 Ok(Err(error)) => Output::Error(error),
                 Err(_) => unreachable!(),
-            })
+            });
+            self.output_find_invalidated = true;
         }
 
         let theme = syntax_highlighting::CodeTheme::from_style(&ctx.style());
@@ -178,34 +190,150 @@ impl WindowState for Sandbox {
 
                     match output {
                         Output::ResultXml(xml) => {
-                            egui::ScrollArea::vertical().show(ui, |ui| {
-                                ui.add_sized(
-                                    ui.available_size(),
-                                    egui::TextEdit::multiline(&mut xml.as_str())
-                                        .layouter(&mut layouter)
-                                        .code_editor(),
-                                )
-                            });
+                            let top = ui.next_widget_position();
 
-                            if let Some(needle) = self.output_find_box.as_mut() {
-                                let mut corner_ui = ui.new_child(
-                                    egui::UiBuilder::new()
-                                        .max_rect(egui::Rect::from_min_max(
-                                            ui.max_rect().max
-                                                - egui::vec2(
-                                                    ui.available_width(),
-                                                    ui.available_height().max(32.0)
-                                                ),
-                                            ui.max_rect().max 
-                                            // Move slightly to the left so that it doesn't
-                                            // overlap the scroll bar
-                                            - egui::vec2(16.0, 0.0),
-                                        ))
-                                        .layout(egui::Layout::right_to_left(egui::Align::Max)),
+                            ui.with_layout(Layout::bottom_up(egui::Align::Min), |ui| {
+                                ui.add_space(ui.spacing().window_margin.bottom);
+
+                                let mut selection_cursor = None;
+                                let mut do_scroll = false;
+                                let (needle, regex, idx) = &mut self.output_find_box;
+
+                                let text_height = ui.text_style_height(&egui::TextStyle::Body);
+                                let find_size = vec2(ui.available_width(), text_height + 8.0);
+
+                                ui.allocate_ui_with_layout(
+                                    find_size,
+                                    egui::Layout::right_to_left(egui::Align::Center),
+                                    |ui| {
+                                        // This is a right arrow in the egui font
+                                        let button_rr = ui.button("➡");
+                                        // This is a left arrow in the egui font
+                                        let button_lr = ui.button("⬅");
+                                        if !self.output_find_matches.is_empty() {
+                                            ui.label(format!("{}/{}", *idx + 1, self.output_find_matches.len()));
+                                        }
+                                        let text_out = ui
+                                            .centered_and_justified(|ui| {
+                                                TextEdit::singleline(needle)
+                                                    .id_source("sandbox find box text edit")
+                                                    .hint_text("Regex pattern")
+                                                    .return_key(None)
+                                                    .show(ui)
+                                            })
+                                            .inner;
+
+                                        let text_r = text_out.response;
+
+                                        if text_r.changed() || self.output_find_invalidated {
+                                            // FIXME: this is a silent error
+                                            *regex = Regex::new(needle).ok().filter(|_| !needle.is_empty());
+                                            do_scroll = regex.is_some() && text_r.changed();
+
+                                            self.output_find_matches.clear();
+                                            if let (Some(Output::ResultXml(output)), Some(re)) =
+                                                (self.output.as_ref(), regex)
+                                            {
+                                                for m in re.find_iter(output) {
+                                                    self.output_find_matches.push(m.range());
+                                                }
+                                            }
+
+                                            *idx = (*idx).clamp(0, self.output_find_matches.len());
+                                            self.output_find_invalidated = false;
+                                            ui.ctx().request_repaint();
+                                        }
+
+                                        let off = if button_rr.clicked() {
+                                            1
+                                        } else if button_lr.clicked() {
+                                            -1
+                                        } else if text_r.has_focus() && ctx.input(|i| i.key_pressed(egui::Key::Enter)) {
+                                            if ctx.input(|i| i.modifiers.shift) {
+                                                -1
+                                            } else {
+                                                1
+                                            }
+                                        } else {
+                                            0
+                                        };
+
+                                        if off != 0 {
+                                            if let Some(new_idx) = idx.checked_add_signed(off) {
+                                                if new_idx < self.output_find_matches.len() {
+                                                    *idx = new_idx;
+                                                } else {
+                                                    *idx = 0;
+                                                }
+                                            } else {
+                                                *idx = self.output_find_matches.len().saturating_sub(1);
+                                            }
+
+                                            do_scroll = true;
+                                        }
+                                    },
                                 );
 
-                                corner_ui.text_edit_singleline(needle);
-                            };
+                                if let Some(range) = self.output_find_matches.get(*idx).cloned() {
+                                    // why does it work this way??
+                                    let start_cc = xml[..range.start].chars().count();
+                                    let end_cc = start_cc + xml[range.start..range.end].chars().count();
+                                    let ccrange = egui::text::CCursorRange {
+                                        primary: CCursor::new(start_cc),
+                                        secondary: CCursor::new(end_cc),
+                                    };
+                                    selection_cursor = Some(ccrange);
+                                }
+
+                                let mut galley = layouter(ui, xml, ui.available_width());
+                                let selection_crange = selection_cursor.map(|ccrange| egui::text::CursorRange {
+                                    primary: galley.from_ccursor(ccrange.primary),
+                                    secondary: galley.from_ccursor(ccrange.secondary),
+                                });
+
+                                if let (Some(crange), Some((id, Some(mut state)))) = (
+                                    selection_crange.filter(|_| do_scroll),
+                                    self.output_scroll_id.map(|id| (id, scroll_area::State::load(ctx, id))),
+                                ) {
+                                    let scroll_y = galley.pos_from_cursor(&crange.primary).min.y;
+                                    state.offset.y = (scroll_y - ui.available_height() / 2.0)
+                                        .clamp(0.0, galley.size().y - ui.available_height());
+                                    state.store(ctx, id);
+                                }
+
+                                // sequel, this time with match highlighting
+                                let mut layouter2 = |_: &Ui, _: &str, _: f32| {
+                                    if let Some(crange) = selection_crange {
+                                        let mut v = ui.visuals().clone();
+                                        v.selection.bg_fill = Color32::GREEN;
+                                        paint_text_selection(&mut galley, &v, &crange, None);
+                                    }
+                                    galley.clone()
+                                };
+
+                                let output_size = ui.available_size();
+                                // HACK: This manual placement stops egui layout code from completely
+                                //       failing at its job and resizing the output area for an unknown
+                                //       reason.
+                                egui::Area::new(Id::new("I hate egui layout"))
+                                    .movable(false)
+                                    .fixed_pos(top)
+                                    .show(ctx, |ui| {
+                                        ui.set_min_size(output_size);
+                                        ui.set_max_size(output_size);
+
+                                        self.output_scroll_id = Some(
+                                            egui::ScrollArea::vertical()
+                                                .show(ui, |ui| {
+                                                    egui::TextEdit::multiline(&mut xml.as_str())
+                                                        .layouter(&mut layouter2)
+                                                        .code_editor()
+                                                        .show(ui)
+                                                })
+                                                .id,
+                                        );
+                                    });
+                            });
                         }
                         Output::Error(error) => {
                             // This prevents errors from shrinking the panel
