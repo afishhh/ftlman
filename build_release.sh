4478eb8109bd35505b6de3fdcefdc95a121c363d
Hubert Głuchowski <fishhh@fishhh.dev>
2025-01-27 13:11:27 +0100

    Build MacOS binaries in build_release.sh script

--- build_release.sh
+++ build_release.sh
@@ -2,18 +2,32 @@
 
 set -xeuo pipefail
 
-cross +nightly build --target x86_64-unknown-linux-gnu --release
-cross +nightly build -p windows_gui_wrapper --target x86_64-pc-windows-gnu --release
-cross +nightly build --target x86_64-pc-windows-gnu --release
+UNIXLIKES=(
+  x86_64-unknown-linux-gnu
+  x86_64-apple-darwin
+  aarch64-apple-darwin
+)
+
+for target in "${UNIXLIKES[@]}"; do
+  cross +nightly build --target "$target" --release
+  [[ "$target" == *-apple-darwin ]] && {
+    llvm-strip --strip-all "target/$target/release/ftlman"
+  }
+done
+
+cross +nightly build -p windows_gui_wrapper --target-dir target-x86_64-pc-windows-gnu --target x86_64-pc-windows-gnu --release
+cross +nightly build --target-dir target-x86_64-pc-windows-gnu --target x86_64-pc-windows-gnu --release
 
 [[ -e release ]] || mkdir release
 cd release
 
-ln ../target/x86_64-pc-windows-gnu/release/ftlman.exe ftlman.exe
-ln ../target/x86_64-pc-windows-gnu/release/windows_gui_wrapper.exe ftlman_gui.exe
+ln -f ../target-x86_64-pc-windows-gnu/x86_64-pc-windows-gnu/release/ftlman.exe ftlman.exe
+ln -f ../target-x86_64-pc-windows-gnu/x86_64-pc-windows-gnu/release/windows_gui_wrapper.exe ftlman_gui.exe
 7z a ftlman-x86_64-pc-windows-gnu.zip ftlman_gui.exe ftlman.exe
 rm ftlman_gui.exe ftlman.exe
 
-ln ../target/x86_64-unknown-linux-gnu/release/ftlman ftlman
-tar cvaf ftlman-x86_64-unknown-linux-gnu.tar.gz ftlman
-rm ftlman
+for target in "${UNIXLIKES[@]}"; do
+  ln -f ../target/"$target"/release/ftlman ftlman
+  tar cvaf "ftlman-$target.tar.gz" ftlman
+  rm ftlman
+done
