61198ba176e8e24006dab0b219a03b695822a1cf
Hubert Głuchowski <fishhh@fishhh.dev>
2025-03-22 19:45:04 +0100

    Add more modsets to compare_with_slipstream

---
+++ compare_with_slipstream/modsets/issue-6.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+source modsets/multiverse.sh
+
+download_mod googledrive "System Uncapper" 10HEDAH118eLYsPoajCfIOtbh5fNo3DAF
+download_mod googledrive "Reactor Overcharger" 1SOUHmCXNNmqftR5lq8Lm2X1YqgnMH8aQ
+
+unset PATCHED_SLIPSTREAM_HASH
